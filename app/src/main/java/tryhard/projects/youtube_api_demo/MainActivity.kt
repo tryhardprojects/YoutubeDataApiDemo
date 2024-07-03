c6645ef886be2ed4fa7f144a73f08035c36534c0
you55642 <dk159357852@gmail.com>
2024-07-03 19:03:08 +0800

    Add check google play  service flow

--- app/src/main/java/tryhard/projects/youtube_api_demo/MainActivity.kt
+++ app/src/main/java/tryhard/projects/youtube_api_demo/MainActivity.kt
@@ -1,6 +1,9 @@
 package tryhard.projects.youtube_api_demo
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -8,19 +11,93 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.GoogleApiAvailability
 import tryhard.projects.youtube_api_demo.ui.theme.Youtube_api_demoTheme
 
 class MainActivity : ComponentActivity() {
+
+    companion object {
+        private const val REQUEST_GOOGLE_PLAY_SERVICES = 1002
+    }
+
+    private val isPlayOK = mutableStateOf(false)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        isPlayOK.value = isGooglePlayServicesAvailable()
         setContent {
             Youtube_api_demoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Surface(modifier = Modifier.padding(innerPadding)) {
-                        YoutubeScreen()
+                        if(!isPlayOK.value) {
+                          acquireGooglePlayServices()
+                        } else {
+                            YoutubeScreen()
+                        }
                     }
+                }
+            }
+        }
+    }
+
+
+    /**
+     * Check that Google Play services APK is installed and up to date.
+     * @return true if Google Play Services is available and up to
+     * date on this device; false otherwise.
+     */
+    private fun isGooglePlayServicesAvailable(): Boolean {
+        val apiAvailability =
+            GoogleApiAvailability.getInstance()
+        val connectionStatusCode =
+            apiAvailability.isGooglePlayServicesAvailable(this)
+        return connectionStatusCode == ConnectionResult.SUCCESS
+    }
+
+    /**
+     * Attempt to resolve a missing, out-of-date, invalid or disabled Google
+     * Play Services installation via a user dialog, if possible.
+     */
+    private fun acquireGooglePlayServices() {
+        val apiAvailability =
+            GoogleApiAvailability.getInstance()
+        val connectionStatusCode =
+            apiAvailability.isGooglePlayServicesAvailable(this)
+        if (apiAvailability.isUserResolvableError(connectionStatusCode)) {
+            showGooglePlayServicesAvailabilityErrorDialog(connectionStatusCode)
+        }
+    }
+
+    /**
+     * Display an error dialog showing that Google Play Services is missing
+     * or out of date.
+     * @param connectionStatusCode code describing the presence (or lack of)
+     * Google Play Services on this device.
+     */
+    private fun showGooglePlayServicesAvailabilityErrorDialog(
+        connectionStatusCode: Int
+    ) {
+        val apiAvailability = GoogleApiAvailability.getInstance()
+        val dialog = apiAvailability.getErrorDialog(
+            this@MainActivity,
+            connectionStatusCode,
+            REQUEST_GOOGLE_PLAY_SERVICES
+        )
+        dialog!!.show()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            REQUEST_GOOGLE_PLAY_SERVICES -> {
+                if (resultCode != RESULT_OK) {
+                    Toast.makeText(this, "Google Play Services not available", Toast.LENGTH_LONG).show()
+                } else {
+                    isPlayOK.value = true
                 }
             }
         }
